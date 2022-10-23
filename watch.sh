72d6a508f1122024ccd3789c736888bb90e72854
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-10-23 19:44:17 +0000

    chore: add script to run mvn at watch mode

---
+++ watch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh +x
+apk update && apk add inotify-tools
+place="$1"
+shift
+rest="$@"
+cd "$place"
+while true; do
+  echo '' &&
+  echo '--------------------------------------------------------------------' &&
+  echo '' &&
+  sleep 1 &&
+  inotifywait -m --exclude "target" "$place" -r -e modify|mvn ${rest:-test}
+done
+cd -
+
