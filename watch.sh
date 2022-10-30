263cb6496c282accf2bd2c3203e82a6998d20964
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-10-30 15:02:03 +0000

    chore: update watch script

--- watch.sh
+++ watch.sh
@@ -1,15 +1,15 @@
-#!/bin/sh +x
+#!/bin/sh -x
 apk update && apk add inotify-tools
 place="$1"
 shift
-rest="$@"
+command="$@"
 cd "$place"
 while true; do
   echo '' &&
   echo '--------------------------------------------------------------------' &&
   echo '' &&
   sleep 1 &&
-  inotifywait -m --exclude "target" "$place" -r -e modify|mvn ${rest:-test}
+  inotifywait -m --exclude target $place -r -e modify | $command
 done
 cd -
 
